644074341ac8aeb53d4992c251b71c65ff3af5ea
pandeod <onkardpande@gmail.com>
2018-01-31 23:35:40 +0530

    header changes

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+//import { AppRegistry } from 'react-native';
+//import App from './App';
+
+import React from 'react';
+import {AppRegistry} from 'react-native';
+import Header from './src/components/header';
+
+const App=()=>{
+	return(
+        <Header headerTextToShow={'Albums'}/>
+		);
+};
 
 AppRegistry.registerComponent('albums', () => App);
